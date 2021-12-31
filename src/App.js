973a08591c6c7576c0226506cd1eca5884d2c029
Yashar Akish <yashar_akish@yahoo.com>
2021-12-31 11:59:44 -0500

    creating HomePage component displaying in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+
+import HomePage from './homepage.component';
+
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }

70952dc56dcbe9195cd41d1b3ef9446c40aefab5
Yashar Akish <yashar_akish@yahoo.com>
2021-12-31 17:16:18 -0500

    finishing the styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,14 +6,15 @@ import './menu-item.styles.scss';
 const MenuItem = ({title, imageUrl, size}) =>  {
 
   return(
-    <div 
-      style={{backgroundImage: `url(${imageUrl})`}}
+    <div
       className={`${size} menu-item`}
       > 
       {/* the way we use to apply css style dynamically in react javascript, using ` and url()
       applying two different class css */}
+
+      <div className='background-image' style={{backgroundImage: `url(${imageUrl})`}} />
       <div className='content'>
-        <h1 className='title'>{title}</h1>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitle'>SHOP NOW</span>
       </div>
     </div>

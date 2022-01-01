670322b720e3a8710f88aebe920a74833c0f1751
Yashar Akish <yashar_akish@yahoo.com>
2022-01-01 18:29:31 -0500

    adding Header component for  navigation bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
+import Header  from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 
@@ -10,6 +11,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Routes>
         <Route  path='/' element={<HomePage/>} />
         <Route  path='/shop' element={<ShopPage/>} />

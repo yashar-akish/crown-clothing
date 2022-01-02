a6fdbaa1cce74ffdadd372d9683d3e6cb363e8c2
Yashar Akish <yashar_akish@yahoo.com>
2022-01-01 21:12:26 -0500

    creating signIn component including the design and custom button in signin-signup page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header  from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route  path='/' element={<HomePage/>} />
         <Route  path='/shop' element={<ShopPage/>} />
+        <Route  path='/signin' element={<SignInAndSignUpPage/>} />
       </Routes>
     </div>
   );
